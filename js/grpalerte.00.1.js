72df5bf36d1e39ac92a5988f10a39f31b9df5277
Guillermo <g.rodriguez.p@hotmail.com>
2019-05-16 11:50:09 -0500

    Creando los Eventos de Plugin Completo

--- js/grpalerte.00.1.js
+++ js/grpalerte.00.1.js
@@ -1,13 +1,50 @@
 (function( $ ){
   var methods = {
 	init : function( options ) { 
+      //codigo
+        var settings = $.extend({
+            //Parametros por Defecto   
+            width:"350px"
+		}, options );
+		$(this).addClass('gp_model')
+		$(this).append($('<div>',{'class':'gp_contener'})
+			.append($('<div>',{'class':'gp_modeltitle'})
+				.append($('<span>',{'class':'gp_ti_title'}))
+					.append($('<span> ',{'class':'gp_ti_close'})))
+					.append($('<div>',{'class':'gp_dialog'})
+				.append($('<div>',{'class':'gp_dialog_popup'})
+					.append($('<div id=\'gp_dialogtext\'>'))
+		))); 
+		$(".gp_contener").css({   width: settings.width,}); 
 	},
 	show : function( options) {
+		var settings = $.extend({
+			mensaje: "Error de Validacion",title: "Sistema",
+		}, options );
+		var div = $(this);
+		$(this).css({ display: 'block', });
+		$(".gp_ti_close").html("x");       
+		$(".gp_ti_close").click( function() { div.css({ display: '', }); });
+		$(".gp_ti_title").html( settings.title  ); 
+		$("#gp_dialogtext").html( settings.mensaje  );
+		$(".gp_dialog_popup").removeClass('gp_dialogicono_carg gp_dialogicono_info'); 
+		$(".gp_dialog_popup").addClass('gp_dialogicono_info');		
 	},
     hide : function( ) { 
       $(this).css({ display: '', });
     },	
 	cargando : function( options ) { 
+		var div = $(this);
+		var settings = $.extend({
+          mensaje: "Cargando...",title: "Sistema",
+		}, options );
+		$(this).css({ display: 'block', });
+		$(".gp_ti_close").html("x");       
+		$(".gp_ti_close").click( function() { div.css({ display: '', }); });		  
+		$(".gp_dialog_popup").removeClass('gp_dialogicono_carg gp_dialogicono_info');
+		$(".gp_dialog_popup").addClass('gp_dialogicono_carg');
+		$(".gp_ti_title").html( settings.title  ); 
+		$("#gp_dialogtext").html( settings.mensaje  );  	
 	}
   };
   $.fn.grpAlert = function( method ) {
